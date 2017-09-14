b359f93ee672dd04960cd0fe205bc6adab9d9950
Tom SALVADORE <tom.salvadore@gmail.com>
2017-09-14 11:34:19 +0200

    Creation du modele membres

---
+++ model/modelMembre.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+var co_db = require('./coDB');
+
+var co = co_db.connection();
+
+exports.identification = function(mail, pass, callback) {
+    
+    var query = "SELECT * FROM membres WHERE mail = ? AND pass = ?";
+    var inserts = [mail, pass];
+    query = mysql.format(query, inserts);
+    
+    co.query(query, function (error, results, fields) {
+        if (error) throw error;
+        
+        callback(results);
+    });
+    
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TBB' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'TBB'});
+});
+
+router.post('/', function (req, res) {
+    
+    var mail = req.body.mail;
+    var pass = req.body.pass;
+    
+    //APPEL DU MODEL - CO BDD / REQS
+    var modelMembres = require('../model/modelMembre');
+    
+    modelMembres.identification(mail, pass, function (datas) {
+        
+        //SI LA REQUETE RENVOIE UN RESULTAT ON CREE LA SESSION
+        if (datas.length > 0) {
+            console.log("Connecté ! => "+datas);
+        }
+    });
 });
 
 module.exports = router;
