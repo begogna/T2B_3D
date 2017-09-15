5410f05d380425ed3ace800cba73007c55946f63
Tom SALVADORE <tom.salvadore@gmail.com>
2017-09-15 10:47:40 +0200

    Modif barre menu + controller pour l'inscription

--- public/javascripts/inscriptionController.js
+++ public/javascripts/inscriptionController.js
@@ -1,5 +1,19 @@
 var inscriptionController = function () {
+    
+    var btnSubmit = $('#btnSubmit');
+    var divErrorInscr = $('');
+    
 
-
+    function initializeForm(){
+    
+    }
+    
+    return {
+    
+    }
 
 }();
+
+$(function() {
+
+});
