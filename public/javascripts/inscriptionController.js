22947687a3968a04ca6d5c531380e60a0fe79087
Tom SALVADORE <tom.salvadore@gmail.com>
2017-09-15 09:31:46 +0200

    Création de inscriptionController + quelques petites modifs

---
+++ public/javascripts/inscriptionController.js
@@ -0,0 +1,5 @@
+var inscriptionController = function () {
+
+
+
+}();
--- routes/catalogue.js
+++ routes/catalogue.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('catalogue', { title: 'Catalogue TBB' });
+    sess = req.session;
+    
+    if (sess.errCO) {
+        res.render('catalogue', {title: 'TBB', errCo: true, co: false, nomMembre: null});
+    }
+    else if (sess.mail) {
+        
+        //APPEL DU MODEL - CO BDD / REQS
+        var modelMembres = require('../model/modelMembre');
+        
+        modelMembres.selectByMail(sess.mail, function (datas) {
+            res.render('catalogue', {title: 'TBB', errCo: false, co: true, nomMembre: datas[0]['prenom']});
+        });
+    }
+    else {
+        res.render('catalogue', {title: 'TBB', errCo: false, co: false, nomMembre: null});
+    }
 });
 
 module.exports = router;
