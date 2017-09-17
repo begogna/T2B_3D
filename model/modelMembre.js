17de9cb5ff023e16d345c3a1a245f88017da3d0c
Tom SALVADORE <tom.salvadore@gmail.com>
2017-09-17 14:16:30 +0200

    Realisation du module d'inscription

--- model/modelMembre.js
+++ model/modelMembre.js
@@ -30,3 +30,17 @@ exports.selectByMail = function(mail, callback) {
     });
     
 };
+
+exports.inscription = function(nom, prenom, mail, pass) {
+    
+    var date = new Date();
+    
+    var sql = "INSERT INTO membres(nom, prenom, mail, pass, dateInscr) VALUES ?";
+    var values = [
+        [nom, prenom, mail, pass, date]
+    ];
+    co.query(sql, [values], function(err) {
+        if (err) throw err;
+    });
+    
+};
