7870a974c35905ec510e87f4ebffa94d59e89e40
Begoña Pérez Rodríguez <begmeil@gmail.com>
2017-09-19 09:39:38 +0200

    fixed merge conflits

---
+++ public/javascripts/indexController.js
@@ -0,0 +1,8 @@
+var IndexController = function () {
+
+    $(document).on("click", "button.detail", function () {
+        var id = $(this).attr("data-id"); //on récupère la propriete du dataset qui s'appelle id
+
+    });
+
+}();
